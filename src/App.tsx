e0a05c663d4af0e2621ab7809d8eb4db6f368993
MaryAnzh <maryang@mail.ru>
2022-12-14 18:00:24 +0300

    docs: install plugins

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Apps.scss';
+
+function App() {
+  return (
+    <section>
+      <h2>My App</h2>
+    </section>
+  );
+
+}
+
+export default App;
