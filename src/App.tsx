bd955864f4dc158bde426d96ebf8a7c3d0220644
MaryAnzh <maryang@mail.ru>
2022-12-18 15:43:27 +0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import React from 'react'
 import './Apps.scss'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+} from 'react-router-dom';
+import { ItemsPage } from './pages/items-page/ItemsPage';
+import { CartPage } from './pages/cart-page/CartPage';
+import { NotFoundPage } from './pages/not-found-page/NotFoundPage';
 
+export const App = () => (
+    <Router>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Catalog</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">Cart</Link>
+                    </li>
+                    <li>
+                        <Link to="/notfound">NotFound</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
 
-export const App = (): JSX.Element => (
-    <h2>Online Store</h2>
-)
+        <main>
+            <Routes>
+                <Route path="/" element={<ItemsPage />}></Route>
+                <Route path="/cart" element={<CartPage />}></Route>
+                <Route path="/notfound" element={<NotFoundPage />}></Route>
+            </Routes>
+        </main>
+    </Router>
+);
