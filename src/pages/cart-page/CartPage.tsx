8be3895d909dc4ea428690f6e4f16f9d77698ee8
MaryAnzh <maryang@mail.ru>
2022-12-18 14:51:30 +0300

    refactour: add component templates

---
+++ src/pages/cart-page/CartPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './CartPage.scss'
+
+
+export const CartPage = (): JSX.Element => (
+    <h2>CartPage work</h2>
+)
---
+++ src/pages/item-page/ItemPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './ItemPage.scss'
+
+
+export const ItemPage = (): JSX.Element => (
+    <h2>ItemPage work</h2>
+)
