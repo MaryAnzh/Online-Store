15b3584c1d9ab9e5fbab7f94204d17883581a747
MaryAnzh <maryang@mail.ru>
2022-12-21 12:01:10 +0300

    feat: add title in items page

--- src/pages/items-page/ItemsPage.tsx
+++ src/pages/items-page/ItemsPage.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import { catalog } from '../../core/data/catalog.data';
-import styles from './ItemsPage.scss'
+import './ItemsPage.scss'
 
 
 export const ItemsPage = (): JSX.Element => (
-    <h2>ItemsPage work</h2>
+    <section className='catalog'>
+        <div className='catalog__wrap'>
+            <div className='catalog__wrap__info-wrap'>
+                <div className='catalog__wrap__info-wrap__info'>
+                    <h3 className='catalog__wrap__info-wrap__info__title'>
+                        Добро пожаловать в Online Stor
+                    </h3>
+                    <p className='catalog__wrap__info-wrap__info__text'>
+                        Большой выбор товаров, на любой вкус и кошелек.
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
 )
