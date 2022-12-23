bee014bc1e0dfd9c73068833a0302159aa6fd986
Nikita <nikitosminsk100@gmail.com>
2022-12-23 14:49:11 +0300

    fix: try to fix pull request

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import {ItemsPage} from './pages/items-page/ItemsPage'
-import {CartPage} from './pages/cart-page/CartPage'
-import {NotFoundPage} from './pages/not-found-page/NotFoundPage'
-import {ItemBuyModal} from './containers/item-buy-modal/ItemBuyModal'
-
+import './Apps.scss';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { ItemsPage } from './pages/items-page/ItemsPage'
+import { CartPage } from './pages/cart-page/CartPage'
+import { NotFoundPage } from './pages/not-found-page/NotFoundPage'
+import { Checkbox } from './components/ui/checkbox/Checkbox'
+import { Header } from './containers/header/Header'
+import { Footer } from './containers/footer/Footer'
 
 export const App = (): JSX.Element => (
     <Router>
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Catalog</Link>
-                    </li>
-                    <li>
-                        <Link to="/cart">Cart</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <section className='wrapper'>
+            < Header />
+            <main className='wrapper__main'>
+                <Routes>
+                    <Route path="/" element={<ItemsPage />}></Route>
+                    <Route path="/cart" element={<CartPage />}></Route>
+                    <Route path="/*" element={<NotFoundPage />}></Route>
+                </Routes>
+            </main>
+            < Footer />
 
-        <ItemBuyModal show={true} closeCallback={() => {}}/>
-
-        <main>
-            <Routes>
-                <Route path="/" element={<ItemsPage/>}/>
-                <Route path="/cart" element={<CartPage/>}/>
-                <Route path="/*" element={<NotFoundPage/>}/>
-            </Routes>
-        </main>
+        </section>
     </Router>
 )
