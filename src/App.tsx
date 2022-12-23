7a6dad71c7717ab239e3057473aef7f832a1a3a4
Nikita <nikitosminsk100@gmail.com>
2022-12-23 14:51:35 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import React from 'react'
-import './Apps.scss';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import { ItemsPage } from './pages/items-page/ItemsPage'
-import { CartPage } from './pages/cart-page/CartPage'
-import { NotFoundPage } from './pages/not-found-page/NotFoundPage'
-import { Checkbox } from './components/ui/checkbox/Checkbox'
-import { Header } from './containers/header/Header'
-import { Footer } from './containers/footer/Footer'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {ItemsPage} from './pages/items-page/ItemsPage'
+import {CartPage} from './pages/cart-page/CartPage'
+import {NotFoundPage} from './pages/not-found-page/NotFoundPage'
+import {Checkbox} from './components/ui/checkbox/Checkbox'
 
 export const App = (): JSX.Element => (
     <Router>
-        <section className='wrapper'>
-            < Header />
-            <main className='wrapper__main'>
-                <Routes>
-                    <Route path="/" element={<ItemsPage />}></Route>
-                    <Route path="/cart" element={<CartPage />}></Route>
-                    <Route path="/*" element={<NotFoundPage />}></Route>
-                </Routes>
-            </main>
-            < Footer />
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Catalog</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">Cart</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
 
-        </section>
-
+        <Checkbox onChange={() => {}}/>
+        <main>
+            <Routes>
+                <Route path="/" element={<ItemsPage/>}></Route>
+                <Route path="/cart" element={<CartPage/>}></Route>
+                <Route path="/*" element={<NotFoundPage/>}></Route>
+            </Routes>
+        </main>
     </Router>
 )
