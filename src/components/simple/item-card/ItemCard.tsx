40ba5e91b58e11291190b7556d206b63210dfd55
MaryAnzh <maryang@mail.ru>
2022-12-26 15:50:23 +0300

    feat: add items in catalog

--- src/components/simple/item-card/ItemCard.tsx
+++ src/components/simple/item-card/ItemCard.tsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import './ItemCard.scss';
+import { IItem } from '../../../core/interfaces/catalog.interfaces';
 
-export const ItemCard = (): JSX.Element => (
+type ItemTypeProps = {
+    item: IItem,
+}
+
+export const ItemCard = (props: ItemTypeProps): JSX.Element => (
     <section className='item-card'>
+        <h3>Items Card</h3>
         <div className='item-card__wrap'>
-
         </div>
-
     </section>
 );
--- src/pages/items-page/ItemsPage.tsx
+++ src/pages/items-page/ItemsPage.tsx
@@ -2,36 +2,48 @@ import React from 'react'
 import { catalog } from '../../core/data/catalog.data';
 import './ItemsPage.scss'
 import { ItemCard } from '../../components/simple/item-card/ItemCard';
+import { IItem } from '../../core/interfaces/catalog.interfaces';
 
-export const ItemsPage = (): JSX.Element => (
-    <section className='catalog'>
-        <div className='catalog__wrap'>
-            <section className='catalog__wrap__info-wrap'>
-                <div className='catalog__wrap__info-wrap__info'>
-                    <h3 className='catalog__wrap__info-wrap__info__title'>
-                        Добро пожаловать в Online Stor
-                    </h3>
-                    <p className='catalog__wrap__info-wrap__info__text'>
-                        Большой выбор товаров, на любой вкус и кошелек.
-                    </p>
-                </div>
-            </section>
-            <section className='catalog__wrap__tools-wrap'>
-                <div className='catalog__wrap__tools-wrap__sort'>
-                    Sort
-                </div>
-                <div className='catalog__wrap__tools-wrap__filter'>
-                    Filter
-                </div>
-                <div className='catalog__wrap__tools-wrap__search'>
-                    <input
-                        className='catalog__wrap__tools-wrap__search__input'
-                        type='text' />
-                    <button className='blue-button'>Search</button>
-                </div>
+export const ItemsPage = (): JSX.Element => {
+    const itemCatalog: IItem[] = [...catalog.products];
+    const itemsList: JSX.Element[] = itemCatalog.map(elem => <ItemCard item={elem} />);
 
-            </section>
+    return (
+        <section className='catalog'>
+            <div className='catalog__wrap'>
+                <section className='catalog__wrap__info-wrap'>
+                    <div className='catalog__wrap__info-wrap__info'>
+                        <h3 className='catalog__wrap__info-wrap__info__title'>
+                            Добро пожаловать в Online Stor
+                        </h3>
+                        <p className='catalog__wrap__info-wrap__info__text'>
+                            Большой выбор товаров, на любой вкус и кошелек.
+                        </p>
+                    </div>
+                </section>
+                <section className='catalog__wrap__tools-wrap'>
+                    <div className='catalog__wrap__tools-wrap__sort'>
+                        Sort
+                    </div>
+                    <div className='catalog__wrap__tools-wrap__filter'>
+                        Filter
+                    </div>
+                    <div className='catalog__wrap__tools-wrap__search'>
+                        <input
+                            className='catalog__wrap__tools-wrap__search__input'
+                            type='text' />
+                        <button className='blue-button'>Search</button>
+                    </div>
+                </section>
+                <section className='catalog__wrap__items'>
+                    <h3 className='catalog__wrap__items__title'>Товары</h3>
 
-        </div>
-    </section>
-)
+                    <div className='catalog__wrap__items__items-wrap'>
+                        {itemsList}
+                    </div>
+                </section>
+
+            </div>
+        </section>
+    )
+}
