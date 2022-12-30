31ec3ae00116c80151e91d159030ca930df8f2be
Nikita <nikitosminsk100@gmail.com>
2022-12-30 12:09:47 +0300

    fix: edit styles on main page

--- src/pages/items-page/ItemsPage.tsx
+++ src/pages/items-page/ItemsPage.tsx
@@ -15,10 +15,10 @@ export const ItemsPage = (): JSX.Element => {
                 <section className='catalog__wrap__info-wrap'>
                     <div className='catalog__wrap__info-wrap__info'>
                         <h3 className='catalog__wrap__info-wrap__info__title'>
-                            Welcome to Online Stor
+                            Welcome to Online Store
                         </h3>
                         <p className='catalog__wrap__info-wrap__info__text'>
-                            A big choose of products, for every taste and wallet.
+                            A big choice of products, for every taste and wallet.
                         </p>
                     </div>
                 </section>
@@ -32,7 +32,8 @@ export const ItemsPage = (): JSX.Element => {
                     <div className='catalog__wrap__tools-wrap__search'>
                         <input
                             className='catalog__wrap__tools-wrap__search__input'
-                            type='text' />
+                            type='text'
+                            placeholder="Product search"/>
                         <button className='blue-button'>Search</button>
                     </div>
                 </section>
