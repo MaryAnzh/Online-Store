e8817cceadd3d779a381f5aa238e2ec5933a7e11
MaryAnzh <maryang@mail.ru>
2023-01-06 14:11:06 +0300

    feat: add search

--- src/components/smart/tools-search/ToolsSearch.tsx
+++ src/components/smart/tools-search/ToolsSearch.tsx
@@ -2,7 +2,7 @@ import './ToolsSearch.scss';
 import { IItem } from '../../../core/interfaces/catalog.interfaces';
 import { ItemsQueryOptions } from '../../../core/types/tools.types';
 import { ReactComponent as SearchLogo } from '../../../assets/search.svg';
-import React from 'react';
+import React, { useState } from 'react';
 
 type ToolsSearchProps = {
     toolsSetting: ItemsQueryOptions,
@@ -10,24 +10,33 @@ type ToolsSearchProps = {
 }
 
 export const ToolsSearch = (props: ToolsSearchProps) => {
+    let currentInputValue: string = '';
+    const checkToolsSetting = () => {
+        const search: string | null = props.toolsSetting.search;
+        if (search !== null) {
+            currentInputValue = search;
+        }
+    }
+    checkToolsSetting();
+
     let timerTime = 0;
     let timer: NodeJS.Timer;
+    const [inputValue, setInputValue] = useState(currentInputValue);
 
     const searchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         clearInterval(timer);
         const value = e.target.value;
+        setInputValue(value);
         timer = setInterval(() => {
             if (timerTime < 1000) {
                 timerTime += 100;
             } else {
-                console.log(value);
                 timerTime = 0;
                 props.toolsSetting.search = value;
                 props.modifyItems(props.toolsSetting);
                 clearInterval(timer);
             }
         }, 100);
-
     }
 
     return (
@@ -35,6 +44,7 @@ export const ToolsSearch = (props: ToolsSearchProps) => {
             <input
                 className='tools-search__input'
                 type='text'
+                value={inputValue}
                 onInput={searchInput} />
             <SearchLogo />
         </div>
